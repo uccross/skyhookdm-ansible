dc3997ff22b9dbc8d1a467a47268e3042fc95fe7
Kathryn Dahlgren <kmdahlgr@ucsc.edu>
2019-08-21 17:21:08 -0700

    refactor.

---
+++ ansible/exps/query_files/PRED_QUERY.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# PRED_QUERY.sh
+
+numobjs=$1
+poolname=$2
+matchfile=$3
+timefile=$4
+select_preds=$5
+project_cols=$6
+data_schema=$7
+
+echo "--------------------------------"
+echo "numobjs      = $numobjs"
+echo "poolname     = $poolname"
+echo "matchfile    = $matchfile"
+echo "timefile     = $timefile"
+echo "select_preds = $select_preds"
+echo "project_cols = $project_cols"
+echo "data_schema  = $data_schema"
+
+{ time bin/run-query --num-objs $numobjs --pool $poolname --wthreads 1 --qdepth 10 --query flatbuf --select "$select_preds" --project-cols $project_cols --data-schema "$data_schema" > $matchfile ; } 2>> $timefile ;
