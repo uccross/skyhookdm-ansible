3a5eef041750b1570c0d1682edb1e8075676a0c6
Kathryn Dahlgren <kmdahlgr@ucsc.edu>
2019-08-23 21:21:48 -0700

    adding another test. checkpoint save.

--- ansible/exps/query_files/PRED_QUERY.sh
+++ ansible/exps/query_files/PRED_QUERY.sh
@@ -8,14 +8,25 @@ timefile=$4
 select_preds=$5
 project_cols=$6
 data_schema=$7
+use_cls=$8
 
-echo "--------------------------------"
-echo "numobjs      = $numobjs"
-echo "poolname     = $poolname"
-echo "matchfile    = $matchfile"
-echo "timefile     = $timefile"
-echo "select_preds = $select_preds"
-echo "project_cols = $project_cols"
-echo "data_schema  = $data_schema"
+echo "--------------------------------" ;
+echo "numobjs      = $numobjs" ;
+echo "poolname     = $poolname" ;
+echo "matchfile    = $matchfile" ;
+echo "timefile     = $timefile" ;
+echo "select_preds = $select_preds" ;
+echo "project_cols = $project_cols" ;
+echo "data_schema  = $data_schema" ;
+echo "use_cls      = $use_cls" ;
 
-{ time bin/run-query --num-objs $numobjs --pool $poolname --wthreads 1 --qdepth 10 --query flatbuf --select "$select_preds" --project-cols $project_cols --data-schema "$data_schema" > $matchfile ; } 2>> $timefile ;
+if [ "$use_cls" = True ]
+then
+  echo "using cls." ;
+  echo "{ time bin/run-query --num-objs $numobjs --pool $poolname --wthreads 1 --qdepth 10 --query flatbuf --select "$select_preds" --project-cols $project_cols --data-schema \"$data_schema\" --use-cls > $matchfile ; }" >> $timefile ;
+  { time bin/run-query --num-objs $numobjs --pool $poolname --wthreads 1 --qdepth 10 --query flatbuf --select "$select_preds" --project-cols $project_cols --data-schema "$data_schema" --use-cls > $matchfile ; } 2>> $timefile ;
+else
+  echo "{ time bin/run-query --num-objs $numobjs --pool $poolname --wthreads 1 --qdepth 10 --query flatbuf --select "$select_preds" --project-cols $project_cols --data-schema \"$data_schema\" > $matchfile ; }" >> $timefile ;
+  echo "not using cls." ;
+  { time bin/run-query --num-objs $numobjs --pool $poolname --wthreads 1 --qdepth 10 --query flatbuf --select "$select_preds" --project-cols $project_cols --data-schema "$data_schema" > $matchfile ; } 2>> $timefile ;
+fi
