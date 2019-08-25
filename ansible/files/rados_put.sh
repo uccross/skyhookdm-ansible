c32bfb9f6e6b0378c4392aaa62a24f8e2f6cf222
Kathryn Dahlgren <kmdahlgr@ucsc.edu>
2019-08-25 12:05:56 -0700

    make sure these work.

--- ansible/files/rados_put.sh
+++ ansible/files/rados_put.sh
@@ -3,4 +3,4 @@ pool_name=$1
 format_secondary_device=$2
 filename=$3
 objnum=$4
-./rados-store-glob.sh $pool_name /mnt/$format_secondary_device/$filename $objnum ;
+sudo bash rados-store-glob.sh $pool_name /mnt/$format_secondary_device/$filename $objnum ;
--- ansible/files/scp_and_write_data.py
+++ ansible/files/scp_and_write_data.py
@@ -8,25 +8,33 @@ this_user               = sys.argv[1]
 data_node_addr          = sys.argv[2]
 data_path               = sys.argv[3]
 data_dir                = sys.argv[4]
-format_secondary_device = sys.argv[5]
-pool_name               = sys.argv[6]
+data_file_prefix        = sys.argv[5]
+data_file_suffix        = sys.argv[6]
+format_secondary_device = sys.argv[7]
+pool_name               = sys.argv[8]
+numobjs                 = int( sys.argv[9] )
 
 # get file names from remote storage
-remote_files_str = subprocess.check_output(["sudo", "bash", \
-                                            "rsync_command.sh", \
-                                            "kat", \
-                                            data_node_addr, \
-                                            "/mnt/sda4/arity3datasets/flatflex/fbxrows-arity3-1mb-100objs/"])
-remote_files_str = remote_files_str.rstrip() ;
-remote_files_list = [ f1.split()[-1] for f1 in [ f for f in remote_files_str.split("\n") ] ]
+#remote_files_str = subprocess.check_output(["sudo", "bash", \
+#                                            "rsync_command.sh", \
+#                                            "kat", \
+#                                            data_node_addr, \
+#                                            "/mnt/sda4/arity3datasets/flatflex/fbxrows-arity3-1mb-100objs/"])
+#remote_files_str = remote_files_str.rstrip() ;
+#remote_files_list = [ f1.split()[-1] for f1 in [ f for f in remote_files_str.split("\n") ] ]
 #print remote_files_list
+
+# generate remote files list bc rsync isn't working well with ansible
+remote_files_list = []
+for i in range( 0, numobjs ) :
+  remote_files_list.append( data_file_prefix + str(i) + data_file_suffix )
+
 
 obj_counter=0
 for f in remote_files_list :
   # scp one object file at a time into secondary storage on local
   cmd0 = "su - " + this_user + " -c 'scp -o StrictHostKeyChecking=no -r " + \
-             this_user + "@" + data_node_addr + ":" + data_path + "/" + \
-             data_dir + "/" + f + " /mnt/" + format_secondary_device + "/'" + " ;"
+             this_user + "@" + data_node_addr + ":" + data_path + "/" + data_dir + "/" + f + " /mnt/" + format_secondary_device + "/'" + " ;"
   print cmd0
   os.system( cmd0 )
 
