61c4241956eb29d74ad20d59869a7f8e1fb5e4e2
KDahlgren <kmdahlgr@ucsc.edu>
2019-09-05 18:30:08 -0700

    updated version with group_id

--- ansible/files/rados_put_parallel.py
+++ ansible/files/rados_put_parallel.py
@@ -5,10 +5,16 @@ import sys
 #################
 #   RADOS PUT   #
 #################
-def rados_put( poolname, obj_name, data_file_fullpath ) :
-  cmd = "rados -p " + poolname + " put " + obj_name + " " + data_file_fullpath
-  print cmd
-  #os.system( "rados -p " + poolname + " put " + obj_name + " " + data_file_fullpath )
+def rados_put( poolname, obj_name, data_file_fullpath, actually_do_it ) :
+
+  if os.path.isfile( data_file_fullpath ) :
+    cmd = "rados -p " + poolname + " put " + obj_name + " " + data_file_fullpath
+    print cmd
+    if actually_do_it :
+      os.system( "rados -p " + poolname + " put " + obj_name + " " + data_file_fullpath )
+  else :
+    print "could not find file '" + data_file_fullpath + "'"
+    sys.exit(0)
 
 ############
 #   MAIN   #
@@ -16,26 +22,38 @@ def rados_put( poolname, obj_name, data_file_fullpath ) :
 def main() :
 
   # inputs
-  max_num_objs = int( sys.argv[1] )
-  poolname     = sys.argv[2]
-  data_file    = os.path.abspath( sys.argv[3] )
+  num_objs       = int( sys.argv[1] )
+  poolname       = sys.argv[2]
+  data_file      = os.path.abspath( sys.argv[3] )
+  actually_do_it = sys.argv[4]
+  group_id       = int( sys.argv[5] )
+
+  if actually_do_it == "True" :
+    actually_do_it = True
+  else :
+    actually_do_it = False
+
+  start_id = group_id*num_objs
+  max_num_objs = start_id + num_objs
 
   job_args_list = []
   base_name     = "obj."
-  for i in range( 0, max_num_objs ) :
-    job_args_list.append( ( poolname, base_name+str(i), data_file ) )
+  for i in range( 0, num_objs ) :
+    job_args_list.append( ( poolname, base_name+str(start_id+i), data_file, actually_do_it ) )
 
   # printing main program process id
   print("ID of main process: {}".format(os.getpid()))
   jobs = []
   if len(job_args_list) < 10 :
     max_k = len(job_args_list)
+    max_concurrent = len(job_args_list)
   else :
     max_k = len(job_args_list)/10
-  obj_id = 0
-  for k in range( 0, (len(job_args_list)/10) ) :
-    for i in range( 0, 10 ) :
-      job_args = job_args_list[obj_id]
+    max_concurrent = 10
+  obj_count = 0
+  for k in range( 0, max_k ) :
+    for i in range( 0, max_concurrent ) :
+      job_args = job_args_list[obj_count]
       print "======================================================"
       print job_args
       print "======================================================"
@@ -43,7 +61,7 @@ def main() :
       jobs.append( j )
       j.start()
       print( "ID of process : {}".format( j.pid ) )
-      obj_id += 1
+      obj_count += 1
 
   # wait until processes finish
   for j in jobs :
